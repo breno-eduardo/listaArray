9828d92918fa8043d1622acc66846778109f10cd
Breno Eduardo <brenoeduardo2611@gmail.com>
2024-09-24 10:50:59 -0300

    Anotação de como usar um array

---
+++ lista.js
@@ -0,0 +1,13 @@
+/*let listaGenerica = [];
+let linguagemDeProgramacao = ['javascript', 'c', 'c++','kotlin', 'python'];
+
+linguagemDeProgramacao.push('java', 'ruby', 'golang');
+
+console.log(linguagemDeProgramacao);
+*/
+
+let nome = ['breno', 'bryan', 'gessica','eduardo', 'gael', 'maria'];
+
+console.log(nome[0]);
+console.log(nome[1]);
+console.log(nome[2])
